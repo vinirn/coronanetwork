5d9ab4f6f8396859ddf9342d1e4ee0ceb632248e
Marcos <vini.rn@gmail.com>
2020-04-01 16:44:57 -0400

    get wikipedia list of cities

---
+++ getcities.py
@@ -0,0 +1,49 @@
+import sys
+import urllib.request, urllib.error, urllib.parse
+from stringUtils import getQuotesParam
+
+csvfilename = "cities.csv"
+csvfile = open(csvfilename, 'w')
+
+url = 'https://pt.wikipedia.org/wiki/Lista_de_munic%C3%ADpios_do_Brasil_por_popula%C3%A7%C3%A3o'
+
+print("Getting URL...")
+response = urllib.request.urlopen(url)
+webContent = str(response.read())
+
+pos = webContent.find(r'<table class="wikitable sortable" style="text-align: center;">')
+num=0
+while True:
+
+    lineStart = webContent.find(r'<tr>',pos)
+    if lineStart<1:
+        break
+    pos = lineStart
+
+    num+=1
+    print(num,end='\t')
+
+    href, pos = getQuotesParam(webContent,"href",pos)
+
+    cityName, pos = getQuotesParam(webContent,"title",pos)
+    cityPrint = cityName.ljust(32,' ')
+    print(cityPrint,end='\t')
+
+    stateName, pos = getQuotesParam(webContent,"title",pos)
+    stateName = stateName.replace(" (estado)","")
+    statePrint = stateName.ljust(20,' ')
+    print(statePrint,end='\t')
+
+    token = r'<td style="text-align:right;">'
+    paramStart = webContent.find(token,pos)
+    paramEnd = webContent.find(r'\n</td>',paramStart+len(token))
+    population = webContent[(paramStart+len(token)):(paramEnd)]
+    print(population,end='\t')
+
+    print(href,end='\t')
+
+    csvfile.write(str(num)+";"+cityName+";"+stateName+";"+str(population)+";"+href+"\n")
+
+    print()
+
+csvfile.close()
---
+++ stringUtils.py
@@ -0,0 +1,24 @@
+def replaceURLEncoding(content):
+    newcontent = content
+    newcontent = newcontent.replace(r'&#39;',"'")
+    newcontent = newcontent.replace(r'\xc3\x81',r'Á')
+    newcontent = newcontent.replace(r'\xc3\x8d',r'Í')
+    newcontent = newcontent.replace(r'\xc3\xa1',r'á')
+    newcontent = newcontent.replace(r'\xc3\xa2',r'â')
+    newcontent = newcontent.replace(r'\xc3\xa3',r'ã')
+    newcontent = newcontent.replace(r'\xc3\xa7',r'ç')
+    newcontent = newcontent.replace(r'\xc3\xa9',r'é')
+    newcontent = newcontent.replace(r'\xc3\xaa',r'ê')
+    newcontent = newcontent.replace(r'\xc3\xad',r'í')
+    newcontent = newcontent.replace(r'\xc3\xb3',r'ó')
+    newcontent = newcontent.replace(r'\xc3\xb4',r'ô')
+    newcontent = newcontent.replace(r'\xc3\xb5',r'õ')
+    newcontent = newcontent.replace(r'\xc3\xba',r'ú')
+    return(newcontent)
+
+def getQuotesParam(content,param,startpos):
+    paramStart = content.find(param+r'="',startpos)
+    paramEnd = content.find(r'"',paramStart+len(param)+2)
+    paramValue = content[(paramStart+len(param)+2):(paramEnd)]
+    paramValue = replaceURLEncoding(paramValue)
+    return(paramValue, paramEnd)
